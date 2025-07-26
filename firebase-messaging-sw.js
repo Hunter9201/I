4ad26a60b8a481aef7cbac2adba295d65be786b7
Hunter9201 <LionHunter920@gmail.com>
2025-07-26 12:01:01 +0300

    Update firebase-messaging-sw.js

--- firebase-messaging-sw.js
+++ firebase-messaging-sw.js
@@ -1,1 +1,15 @@
-https://hunter9201.github.io/I/firebase-messaging-sw.js
+// firebase-messaging-sw.js
+importScripts('https://www.gstatic.com/firebasejs/10.12.2/firebase-app.js');
+importScripts('https://www.gstatic.com/firebasejs/10.12.2/firebase-messaging.js');
+
+firebase.initializeApp({
+  apiKey: "AIzaSyABgKrFpiFqC9gndXaK09MvhAVhZHAGk78",
+  authDomain: "lionhunterwebpush.firebaseapp.com",
+  projectId: "lionhunterwebpush",
+  storageBucket: "lionhunterwebpush.firebasestorage.app",
+  messagingSenderId: "803161144360",
+  appId: "1:803161144360:web:6d64c6c5c275d894a4891d",
+  measurementId: "G-GTJFDE1592"
+});
+
+const messaging = firebase.messaging();
